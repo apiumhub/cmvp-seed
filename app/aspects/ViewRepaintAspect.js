d16f6931318f21c0d2dcf55dfe709d774657229d
kmruiz <masruizkevin@gmail.com>
2014-10-28 13:26:13 +0100

    resolved angular digest problem

--- app/aspects/ViewRepaintAspect.js
+++ app/aspects/ViewRepaintAspect.js
@@ -8,8 +8,9 @@ app.registerService(function (container) {
         weave: function (view) {
             meld.after(view, /^[a-z].+/, function () {
                 if (view.$scope.$apply) {
-                    // ignore the error, it's always an 'already in progress error' which doesn't harm.
-                    view.$scope.$apply();
+                    if (!view.$scope.$$phase) {
+                        view.$scope.$apply();
+                    }
                 } else {
                     throw new Error("weaved a view that doesn't have the angular $scope");
                 }
