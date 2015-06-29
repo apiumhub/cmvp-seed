7bcfc5867cdb1c775b6168fc30c3a166dec2b1f6
José manuel Barroso Galindo <theypsilon@gmail.com>
2015-06-29 12:05:50 +0700

    Fixing task

--- grunt/tasks/dev-deploy.js
+++ grunt/tasks/dev-deploy.js
@@ -2,5 +2,5 @@
  * Created by kevin on 2/9/15.
  */
 module.exports = function (grunt) {
-    grunt.registerTask('dev-deploy', ['less:compile', 'concurrent:dev-deploy']);
+    grunt.registerTask('dev-deploy', ['less:compile', 'requirejs:compile', 'concurrent:dev-deploy']);
 };
