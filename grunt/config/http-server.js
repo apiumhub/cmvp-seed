cfe197a247c80d4344f540f6fea154965f464cb8
José manuel Barroso Galindo <theypsilon@gmail.com>
2016-07-27 12:32:33 +0700

    Merge commit '6e7320d0e640b270e7762d9a017eff7af9deec92'
    # Conflicts:
    #	package.json

--- grunt/config/http-server.js
+++ grunt/config/http-server.js
@@ -1,3 +1,16 @@
 /**
- * Created by jose on 27/07/2016.
+ * Created by kevin on 2/9/15.
  */
+module.exports = function(grunt) {
+    return {
+        dev: {
+            root: '.',
+            host: '0.0.0.0',
+            port: '9000',
+            cache: 0, // no cache!
+            showDir: false,
+            autoIndex: false,
+            runInBackground: false
+        }
+    };
+};
